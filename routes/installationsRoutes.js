6cab6ea855005db3c6124847e71979d50e5734e5
cleanup <cleanup@local>
2026-06-28 19:34:15 +0100

    Cleanup: remove non-game products, keep game + auth/Claude/AWS/Stripe infra

--- routes/installationsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
-const { listInstallations } = require("../controllers/installationsController");
-
-router.get("/", protect, listInstallations);
-
-module.exports = router;
